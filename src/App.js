d4bbcd87f98f1a79ea672611edc105a43f38462d
Dragos Ionita WSLu <dragosionitac@gmail.com>
2022-10-06 18:26:58 -0700

    dummy data, context provider, and navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from './pages';
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 const App = () => {
-  const activeMenu = false;
+  const {activeMenu} = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
